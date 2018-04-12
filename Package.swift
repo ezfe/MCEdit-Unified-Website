0f102fed46ba718310c17e656efd9fd46e0d6f48
Ezekiel Elin <ezekielelin@me.com>
2018-04-11 23:37:26 -0400

    Removed auth

--- Package.swift
+++ Package.swift
@@ -6,12 +6,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor-community/markdown.git", from: "0.4.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf", "Authentication", "FluentSQLite", "SwiftMarkdown"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentSQLite", "SwiftMarkdown"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
