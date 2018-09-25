aaac31d4308d5cf98e6cfeea0c3aa34c497d5f21
Ezekiel Elin <ezekielelin@me.com>
2018-09-25 15:40:45 -0400

    Testing out authentication

--- Package.swift
+++ Package.swift
@@ -7,10 +7,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor-community/markdown.git", from: "0.4.0")
+        .package(url: "https://github.com/vapor-community/markdown.git", from: "0.4.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "SwiftMarkdown"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "SwiftMarkdown", "Authentication"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
---
+++ Sources/App/Models/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  App
+//
+//  Created by Ezekiel Elin on 9/25/18.
+//
+
+import Foundation
+import Vapor
+import FluentPostgreSQL
+import Authentication
+
+struct User {
+    
+}
