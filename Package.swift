11fea47c88a54a9caeeb2c1a815ea41ff9769faf
Ezekiel Elin <ezekielelin@me.com>
2018-09-29 20:55:08 -0400

    Revert "Added Redis"
    This reverts commit 594120229f1142e89728f1fff819e2e2aafd60da.

--- Package.swift
+++ Package.swift
@@ -7,13 +7,12 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/redis.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor-community/markdown.git", from: "0.4.0"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
         .package(url: "https://github.com/ezfe/pwnedpasswords-provider.git", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "Redis", "SwiftMarkdown", "Authentication", "PwnedPasswords"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "SwiftMarkdown", "Authentication", "PwnedPasswords"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
