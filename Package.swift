e6213f4bb518eb8ecd7e77a63f26aeb3aa4d0ea0
Ezekiel Elin <ezekielelin@me.com>
2019-03-04 17:17:26 -0500

    Updated Packages

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 let package = Package(
     name: "MCEditWebsite",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor-community/markdown.git", from: "0.4.0"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.3"),
         .package(url: "https://github.com/ezfe/pwnedpasswords-provider.git", from: "1.0.0"),
     ],
     targets: [
