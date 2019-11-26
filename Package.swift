83f71a78ce9cd4fe5be0e4aa19da4edb059927fd
Ezekiel Elin <ezekielelin@me.com>
2019-11-26 09:42:53 -0500

    More attempts to fix build:

--- Package.swift
+++ Package.swift
@@ -4,17 +4,16 @@ import PackageDescription
 let package = Package(
     name: "MCEditWebsite",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.1"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/redis.git", from: "3.3.1"),
-        .package(url: "https://github.com/johnsundell/ink.git", from: "0.1.0"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.3"),
+        .package(url: "https://github.com/vapor/redis.git", from: "3.4.0"),
+        .package(url: "https://github.com/johnsundell/ink.git", from: "0.1.1"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.4"),
         .package(url: "https://github.com/ezfe/pwnedpasswords-provider.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "Redis", "Ink", "Authentication", "PwnedPasswords"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"]),
+        .target(name: "Run", dependencies: ["App"])
     ]
 )
