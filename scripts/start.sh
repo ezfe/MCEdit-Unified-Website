3b35bce9336d494398f40d6174a39b2dacc7cd35
Ezekiel Elin <ezekielelin@me.com>
2021-04-25 21:14:28 -0400

    Properly start node server

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-which svelte-kit
-if [[ $PORT ]]; then
-	echo "Starting on port: $PORT"
-	svelte-kit start --port $PORT --host 0.0.0.0
-else
-	svelte-kit start
-fi
+svelte-kit build
+node ./build
